da0a3135f993551435e777e179450721968d58d2
Raj Mall <rajmall.0206@gmail.com>
2025-01-12 08:53:32 +0530

    UpdateDOM Function

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
+const apiKey='d1095dbc3cf6403da6565612241212';
+const baseUrl='http://api.weatherapi.com/v1';
 
 async function getWeather(city){
-
-    const apiKey='d1095dbc3cf6403da6565612241212';
-    const baseUrl='http://api.weatherapi.com/v1';
     const url=`${baseUrl}/current.json?key=${apiKey}&q=${city}&aqi=no`;
-
     let x= await fetch(url);
+    
     if(!x.ok){
         alert("Unfortunately, we dont have data about this place");
     }
     else{
         let data= await x.json();
-        const bottomDiv=document.getElementById('bottom');
-        bottomDiv.style.display="block";
-    
-        document.querySelector("#temp").innerHTML=Math.round(data.current.temp_c)+"°C";
-        document.querySelector("#city").innerHTML=data.location.name;
-        document.querySelector("#humidity").innerHTML=data.current.humidity+"%";
-        document.querySelector("#wind").innerHTML=data.current.wind_kph+" km/h";
+        updateDOM(data);
     }
+}
+
+function updateDOM(){
+    const bottomDiv=document.getElementById('bottom');
+    bottomDiv.style.display="block";
+
+    document.querySelector("#temp").innerHTML=Math.round(data.current.temp_c)+"°C";
+    document.querySelector("#city").innerHTML=data.location.name;
+    document.querySelector("#humidity").innerHTML=data.current.humidity+"%";
+    document.querySelector("#wind").innerHTML=data.current.wind_kph+" km/h";
 }
 
 const btn=document.querySelector("#btn");
